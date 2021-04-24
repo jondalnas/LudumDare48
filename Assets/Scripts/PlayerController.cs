95e78b79253d3d6dff5f795e52b381eac6383432
Jonas Jensen <jdudal11@gmail.com>
2021-04-24 17:14:02 +0200

    Added scythe and animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,23 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rb;
 	public int speed = 5;
 
+	private Transform scytheLocation;
+	private Transform scythe;
+	public float scytheCooldown = 2f;
+	private float scytheCooldownTimer;
+
+	private Animator anim;
+
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
+		scytheLocation = transform.Find("Sprite").Find("Player Arm").Find("Scythe location");
+		scythe = transform.Find("Sprite").Find("Scythe");
+
+		anim = GetComponent<Animator>();
 	}
 
 	void Update() {
+		//Look at mouse
 		Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 		Vector3 toMouse = mouseWorldPos - transform.position;
@@ -18,6 +30,18 @@ public class PlayerController : MonoBehaviour {
 		rot.y = 0;
 		transform.rotation = rot;
 
+		//Player movement
 		rb.velocity = (Input.GetAxis("Horizontal") * Vector2.right + Input.GetAxis("Vertical") * Vector2.up) * speed;
+
+		//Swing scythe
+		scytheCooldownTimer += Time.deltaTime;
+		if (Input.GetButtonDown("Attack") && scytheCooldownTimer > scytheCooldown) {
+			anim.SetTrigger("Attack");
+
+			scytheCooldownTimer = 0;
+		}
+
+		//Handle scythe location
+		scythe.position = scytheLocation.position;
 	}
 }
